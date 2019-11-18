b21c79e80f79f42d5ffa2da3336eda3aed427e34
Madhuri <madhuris12345@gmail.com>
2019-11-17 21:09:00 -0600

    Project code

---
+++ TwitterDataAnalysis/src/main/scala/Test.scala
@@ -0,0 +1,37 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import java.io._
+
+import org.apache.commons.io.FileUtils
+
+import scala.collection.JavaConversions._
+import scala.io.Source
+
+
+object Test {
+  def main(args: Array[String]): Unit = {
+
+
+
+
+
+    /**
+     * read conclusion file , add positive reviews for genre movies together and compare all of it. WHichever is highest will be popular this year
+     * @param
+     */
+
+    object Foo { var i = 0 }
+      def counter {
+
+         Foo.i += 1
+         println("Invoked %d times".format(Foo.i))
+         }
+
+
+    counter
+    counter
+
+
+
+  }
+
+}
